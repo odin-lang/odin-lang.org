8b9dcff23506d09d4444ceb0a7d957bade2c9b38
skytrias <skytrias@protonmail.com>
2023-07-10 22:41:50 +0200

    add new mixed arguments

--- themes/odin/static/js/script.js
+++ themes/odin/static/js/script.js
@@ -1,15 +1,17 @@
 window.addEventListener('DOMContentLoaded', () => {
 	const observerForTableOfContentActiveState = new IntersectionObserver(entries => {
-		entries.forEach(entry => {
+		for (let i = 0; i < entries.length; i += 1) {
+      var entry = entries[i]
 			const id = entry.target.getAttribute('id')
 
-			if (entry.intersectionRatio > 0) {					
+      if (entry.isIntersecting) {
 				clearActiveStatesInTableOfContents()				
-				var anchor = document.querySelector(`nav li a[href="#${id}"]`)
+				
+        var anchor = document.querySelector(`nav li a[href="#${id}"]`)
 				anchor.parentElement.classList.add('active')
 				anchor.parentElement.scrollIntoView({ block: "nearest" })
 			}
-		})
+		}
 	})		
 
 	document.querySelectorAll('h1[id],h2[id],h3[id],h4[id]').forEach((section) => {

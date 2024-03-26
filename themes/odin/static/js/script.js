881ff67bd987e16324db7b0ba3a4362d5f1cd2f8
gingerBill <bill@gingerbill.org>
2024-03-26 19:17:51 +0000

    Correct indentation

--- themes/odin/static/js/script.js
+++ themes/odin/static/js/script.js
@@ -2,29 +2,29 @@
 window.addEventListener('DOMContentLoaded', () => {
 	const observerForTableOfContentActiveState = new IntersectionObserver(entries => {
 		for (let i = 0; i < entries.length; i += 1) {
-      var entry = entries[i]
-			const id = entry.target.getAttribute('id')
+			var entry = entries[i];
+			const id = entry.target.getAttribute('id');
 
-      if (entry.isIntersecting) {
-				clearActiveStatesInTableOfContents()				
+			if (entry.isIntersecting) {
+				clearActiveStatesInTableOfContents();
 				
-        var anchor = document.querySelector(`nav li a[href="#${id}"]`)
-				anchor.parentElement.classList.add('active')
-				anchor.parentElement.scrollIntoView({ block: "nearest" })
+				var anchor = document.querySelector(`nav li a[href="#${id}"]`);
+				anchor.parentElement.classList.add('active');
+				anchor.parentElement.scrollIntoView({ block: "nearest" });
 			}
 		}
 	})		
 
 	document.querySelectorAll('h1[id],h2[id],h3[id],h4[id]').forEach((section) => {
-		observerForTableOfContentActiveState.observe(section)
-	})
+		observerForTableOfContentActiveState.observe(section);
+	});
 })
 
 // removes all active states
 function clearActiveStatesInTableOfContents() {
 	document.querySelectorAll('nav li').forEach((section) => {
-		section.classList.remove('active')
-	})
+		section.classList.remove('active');
+	});
 }
 
 // add the bootstrap table class property for styling - could maybe just style tables custom
@@ -32,7 +32,7 @@ window.addEventListener('DOMContentLoaded', () => {
 	document.querySelectorAll('table').forEach((table) => {
 		// only non styled tables
 		if (table.className === "") {
-			table.classList.add("table", "table-striped")
+			table.classList.add("table", "table-striped");
 		}
 	})
 })
